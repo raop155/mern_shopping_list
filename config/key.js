78643163843bb09ca925ba09f25c78d4040c39f3
Ricardo Olarte <raop155@hotmail.com>
2020-10-04 15:10:17 -0500

    Feature(MongoDB) Connect MongoDB Database

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://raop:raopdb@development.dphsj.mongodb.net/dbname?retryWrites=true&w=majority',
+};
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
-const express = require('mongoose');
+const mongoose = require('mongoose');
 
 const app = express();
 
 // Body parser
 app.use(express.json());
+
+// DB Config
+
+const db = require('./config/key').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB Connected...');
+  })
+  .catch((error) => console.log(error));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log('Server started in port ' + port));
