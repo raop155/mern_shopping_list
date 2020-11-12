b6eecbfa1a6c56b3310aadf9586fed9c332cf433
Ricardo Olarte <raop155@hotmail.com>
2020-11-12 18:11:18 -0500

    Add User routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
 // User Model
 const User = require('../../models/User');
 
 // @route   GET api/users
-// @desc    Register new user
+// @desc    Get all users
 // @access  Public
 router.get('/', (req, res) => {
-  res.send('register');
+  User.find()
+    .sort({ register_date: -1 })
+    .then((users) => res.json({ status: 'success', users }));
+});
+
+// @route   POST api/users
+// @desc    Register new user
+// @access  Public
+router.post('/', (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Simple validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ status: 'error', message: 'Please enter all fields' });
+  }
+
+  // Check for existing user
+
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      return res.status(400).json({ status: 'error', message: 'User already exists' });
+    }
+
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+
+    // Create salt & hash
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+
+        newUser.save().then((user) => {
+          res.json({
+            status: 'success',
+            user: {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+            },
+          });
+        });
+      });
+    });
+  });
 });
 
 module.exports = router;
